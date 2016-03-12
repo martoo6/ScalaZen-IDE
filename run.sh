3766100539af5d1b525c3a839d1dd2b307823198
Martin Silberkasten <martoo6@gmail.com>
2016-03-11 23:12:36 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-./nwjs-v0.12.3-linux-x64/nw ./www
+NWJS_VERSION="v0.12.3"
+OS=$(uname -s)
+((1<<32)) && B='x64' || B='ia32'
+
+if [[ $OS == *"CYGWIN"* ]]; then
+  ./nwjs-$NWJS_VERSION-win-$B/nw ./www
+fi
+
+if [[ $OS == 'Darwin' ]]; then
+  ./nwjs-$NWJS_VERSION-osx-$B/nw ./www
+fi
+
+if [[ $OS == 'Linux' ]]; then
+  ./nwjs-$NWJS_VERSION-linux-$B/nw ./www;
+fi
