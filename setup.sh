71614a8dfb1e34d22fc421bc7f505b64bb8a05da
Martin Silberkasten <martoo6@gmail.com>
2016-04-11 14:06:03 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,6 @@ if [[ $OS == 'Darwin' ]]; then
   fi
   info "Installing Homebrew utilities"
   brew install coreutils
-  brew install grep --with-default-names
   brew install gnu-sed --with-default-names
 fi
 
@@ -42,7 +41,7 @@ if [[ $OS == 'Linux' ]]; then
 fi
 
 if [[ $OS == 'Darwin' ]]; then
-  f=$(ls nwjs-$NWJS_VERSION-osx-* 2>/dev/null | wc -l)
+  f=$(ls nwjs-$NWJS_VERSION-osx-* 2>/dev/null | wc -l | awk {'print $1'})
   if [[ "$f" == '0' ]]; then
       ((1<<32)) && B='x64' || B='ia32'
       info "Installing NW.js $B"
